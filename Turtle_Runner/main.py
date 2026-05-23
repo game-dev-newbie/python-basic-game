68008d2c6e69fcb8f80b539c3b7d306595bfb391
game-dev-newbie <bdat6832@gmail.com>
2026-05-23 14:04:25 +0700

    create turtle_lists

--- Turtle_Runner/main.py
+++ Turtle_Runner/main.py
@@ -1,11 +1,24 @@
 from turtle import Turtle, Screen
 
+
+def set_position(Turtle, x, y):
+    Turtle.penup()
+    Turtle.goto(x, y)
+
+
 screen = Screen()
-screen.setup(width = 600, height = 400)
+screen.setup(width=600, height=400)
 # Pop up a dialog box to ask the user for your turtle's color
-color = screen.textinput(title = "Turtle Color", prompt = "Enter your turtle's color:")
-print(f"Your turtle's color is {color}")
-t = Turtle(shape = "turtle")
-t.color(color)
+# color = screen.textinput(title = "Turtle Color", prompt = "Enter your turtle's color:")
+# print(f"Your turtle's color is {color}")
+
+turtle_colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+distance = 0
+for i in range(6):
+    t = Turtle(shape="turtle")
+    t.color(turtle_colors[i])
+    distance = i * 50
+    set_position(t, x=-260, y=-120 + distance)
+
 
 screen.exitonclick()
